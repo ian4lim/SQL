eef4b2fc04bbdf8583ba8da48970efa8ec597e6e
Ian <100225789+ian4lim@users.noreply.github.com>
2023-06-11 13:41:10 -0400

    tracking consecutive player logins

---
+++ player_activity.sql
@@ -0,0 +1,30 @@
+/*
+Author: Ian Lim
+Script Date: June 11, 2023
+Description: This query reports the fraction of players that logged in for at
+least two consecutive days starting from their first login date.
+*/
+
+# Adding previous_date column to show the last date of login
+WITH a AS(
+    SELECT
+        player_id,
+        device_id,
+        event_date,
+        LAG(event_date) OVER(PARTITION BY player_id ORDER BY event_date) AS previous_date
+    FROM Activity
+),
+
+# Counting how many players have consecutive logins
+b AS(
+    SELECT
+        COUNT(DISTINCT player_id) AS num_players
+    FROM a
+    WHERE previous_date IS NOT NULL
+    AND DATEDIFF(event_date, previous_date) = 1
+)
+
+# Reporting fraction of players with consecutive logins
+SELECT
+    ROUND(((SELECT * FROM b) / COUNT(DISTINCT player_id)), 2) as fraction
+FROM a
