89832bd6666171b47e57125bec6f0af8ef3021ea
Ian <100225789+ian4lim@users.noreply.github.com>
2024-03-20 10:55:15 -0400

    CTEs and UNION ALL to determine movie ratings

--- movie_rating.sql
+++ movie_rating.sql
@@ -1,6 +1,6 @@
 /*
 Author: Ian Lim
-Script Date: June 11, 2023
+Script Date: March 11, 2024
 Description: Find the name of the user who has rated the greatest number of
 movies. In case of a tie, return the lexicographically smaller user name.
 Find the movie name with the highest average rating in February 2020. In case
