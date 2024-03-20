47a50193a9099cf03976939eedd6727fec872b8d
Ian <100225789+ian4lim@users.noreply.github.com>
2024-03-20 10:52:41 -0400

    Aggregating industry groups and displaying emissions

--- industry_pcf_emissions.sql
+++ industry_pcf_emissions.sql
@@ -1,6 +1,6 @@
 /*
 Author: Ian Lim
-Script Date: May 5, 2023
+Script Date: March 5, 2024
 Description: This query returns a table of each industry_group along with the 
 number of companies which are in that industry and a total of the PCFs for that
 industry, rounded to one decimal place. This data is stored in a PostgreSQL
